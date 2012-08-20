6c25585f5b11224d7cc791d19db22c23a5c3ec35
stephenjose <contact@stephenjose.com>
2012-08-20 10:53:03 -0400

    Midterm Exam part 3.6 - Code complete

--- PHPMidTerm/index.php
+++ PHPMidTerm/index.php
@@ -16,6 +16,17 @@ ActiveRecord\Config::initialize(function($cfg)
 });
 /*end active record import for database connection*/
 
+
+//handling adding to db
+if(array_key_exists('add', $_POST)){
+	$oMeal = new Meal;	//create a new object to store purchase values
+	$oMeal->date = $_POST['dateInput'];   //assigning the date field of the post array to the $oMeal object variable
+	$oMeal->members = $_POST['membersInput'];	
+	$oMeal->save();	//save object to database	
+}
+
+
+
 include 'view/list.php';
 
 ?>
