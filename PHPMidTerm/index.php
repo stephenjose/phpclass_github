f0fdb965e7ad9f31c8aa7db709681751a0e1a4e2
stephenjose <contact@stephenjose.com>
2012-08-20 09:51:29 -0400

    Midterm Exam part 3.3.1 - Database re-connected using Active Record method (previous 3.3 commit used old code and is rendered obsolete by this commit).

--- PHPMidTerm/index.php
+++ PHPMidTerm/index.php
@@ -1,6 +1,20 @@
 <?php 
 
-	require_once('model/database.php');
+/*active record import for database connection*/
+require '../ActiveRecord/ActiveRecord.php';
+
+ActiveRecord\Config::initialize(function($cfg)
+{
+	$cfg->set_model_directory('model');
+	$cfg->set_connections(
+			array(
+					'development' => 'mysql://root:@localhost/phpmidterm',
+					'test' => 'mysql://username:password@localhost/test_database_name',
+					'production' => 'mysql://username:password@localhost/production_database_name'
+			)
+	);
+});
+/*end active record import for database connection*/
 
 ?>
 
