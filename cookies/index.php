ee206cd9f0e1dac3e1bac4177db018d975281894
stephenjose <contact@stephenjose.com>
2012-08-21 12:25:50 -0400

    controller logic - kinda broken, grab rich's copy

--- cookies/index.php
+++ cookies/index.php
@@ -29,19 +29,30 @@
 	/*end active record import for database connection*/
 	
 	
-	/*//controller logic
+	//controller logic
 	if(array_key_exists('action', $_POST)){
-		if($_POST['action'] == save){
-			
+		if($_POST['action'] == 'signup'){
+			include 'view/signup.php';
+			exit();
 		}//end if
-	}//end if*/
+		elseif($_POST['action'] == 'save'){
+			addUser($_POST);
+			addCookie($_POST['username']);
+		}//end elseif
+		elseif($_POST['action'] == 'signin'){
+			if(validateUser($_POST)){
+				
+			}//end if
+		}//end elseif
+		
+	}//end if
 	
 	if(array_key_exists('signup', $_POST) && isset($_POST['signup'])){
 		include 'view/signup.php';
 	}//end if	
 	elseif(array_key_exists('save', $_POST) && isset($_POST['save'])){	//check for a value in the $_POST array (ie - has a form been submitted)
 												//this is where the password will be validated
-		if($_POST['pwInput'] == 'Secret55'){
+		if($_POST['password'] == 'Secret55'){
 			
 			setcookie('username', $_POST['nameInput']);		//setcookie creates a cookie file on the local machine
 			$_COOKIE['username'] = $_POST['nameInput'];		//the $_COOKIE array variable is set using hte value from $_POST, so it can be used below (and elsewhere)
--- cookies/view/form.php
+++ cookies/view/form.php
@@ -6,7 +6,7 @@
 	<h2>Please enter your name:</h2>
 	
 	<label for="nameInput">Name: </label><input type="text" name="nameInput" id="nameInput" /><br/>
-	<label for="pwInput">Password: </label><input type="password" name="pwInput" id="pwInput" />  <!-- note password type field for a text field specifically for passwords -->
+	<label for="password">Password: </label><input type="password" name="password" id="password" />  <!-- note password type field for a text field specifically for passwords -->
 	
 	<input type="submit" name="action" value="Save" />
 	<input type="submit" name="action" value="Sign Up" />
